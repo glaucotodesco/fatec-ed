0f6db7fcf07ccdf6b9645c33d2cf0a751ae659d8
Glauco Todesco <glauco.todesco@gmail.com>
2023-10-25 23:04:31 -0300

    hash table

--- 06-Recursividade/src/fatorial.c
+++ 06-Recursividade/src/fatorial.c
@@ -5,7 +5,7 @@ int calcularFatorial(int n) {
     if (n == 0 || n == 1)
         return 1;
     else
-        return n * calcularFatorial(n - 1);
+        return n * calcularFatorial(n - 2);
 }
 
 int main() {
